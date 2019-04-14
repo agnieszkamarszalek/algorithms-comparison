7d3c46b3a281e94646976ca29ce7dfb0f8725c6b
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2019-04-14 16:14:42 +0200

    SelectionSort impl, test

---
+++ src/main/java/com/amarszalek/algorithms_comparison/domain/models/SelectionSort.java
@@ -0,0 +1,21 @@
+package com.amarszalek.algorithms_comparison.domain.models;
+
+import com.amarszalek.algorithms_comparison.domain.interfaces.SortInterface;
+
+public class SelectionSort implements SortInterface {
+
+    @Override
+    public int[] sort(int[] arrayToSort) {
+        int minNumber;
+        for(int index = 0; index < arrayToSort.length; index++) {
+            for(int innerIndex = index + 1; innerIndex < arrayToSort.length; innerIndex++){
+                minNumber = arrayToSort[index];
+                if (arrayToSort[innerIndex] < minNumber) {
+                    arrayToSort[index] = arrayToSort[innerIndex];
+                    arrayToSort[innerIndex] = minNumber;
+                }
+            }
+        }
+        return arrayToSort;
+    }
+}
---
+++ src/test/java/com/amarszalek/algorithms_comparison/domain/models/SelectionSortTest.java
@@ -0,0 +1,18 @@
+package com.amarszalek.algorithms_comparison.domain.models;
+
+import com.amarszalek.algorithms_comparison.domain.models.abstractTestClasses.AbstractSortTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SelectionSortTest extends AbstractSortTest {
+
+    @Test
+    public void shouldSortArray() {
+        //given
+        SelectionSort selectionSort = new SelectionSort();
+        //when
+        int[] sortedArray = selectionSort.sort(super.getArrayToSort());
+        //then
+        Assert.assertArrayEquals(super.getArrayExpected(), sortedArray);
+    }
+}
